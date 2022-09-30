516d8acf67b4a6265b8a5430068ce47e8407efb2
Fietspompje <74879947+ECHekman@users.noreply.github.com>
2022-09-30 20:17:24 +0200

    rpfm-interface
    rpfm-interface starter project

---
+++ rpfm-interface/src/lib.rs
@@ -0,0 +1,80 @@
+#![deny(clippy::all)]
+
+use napi::bindgen_prelude::*;
+use napi_derive::napi;
+
+#[macro_use]
+extern crate napi_derive;
+
+
+use std::path::{PathBuf, Path};
+
+use rpfm_lib::packfile::PackFile;
+use rpfm_lib::packedfile::text::{Text, TextType};
+use rpfm_lib::packedfile::PackedFileType;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+#[napi]
+pub struct PackFileHolder
+{
+    owned_packfile: PackFile,
+}
+
+
+#[napi]
+pub fn open_pack_files(paths: Vec<String>, use_lazy_loading: bool, ignore_mods: bool, lock_packfile: bool) -> PackFileHolder
+{
+    let mut pathsbufs: Vec<PathBuf> = vec![];
+
+    for path in paths.iter() {
+        pathsbufs.push(PathBuf::from(path));
+    }
+
+    let mut ret_holder = PackFileHolder { owned_packfile: PackFile::default() };
+    match PackFile::open_packfiles(&pathsbufs, use_lazy_loading, ignore_mods, lock_packfile)
+    {
+        Ok(pack_file) => 
+        {
+            ret_holder.owned_packfile = pack_file;
+        }
+        Err(_error) =>
+        {
+            //nothing
+        }
+    }
+
+    return ret_holder;
+}
+
+// Source file interface
+#[napi]
+impl PackFileHolder
+{
+    #[napi]
+    pub fn get_all_lua_file_paths(&mut self) -> Vec<String>
+    {    
+        let mut files_vec = Vec::new();
+        let packed_files = self.owned_packfile.get_ref_packed_files_by_type(PackedFileType::Text(TextType::Lua), true);
+
+        packed_files.iter().for_each(|x| files_vec.push(x.get_path().join("/")));
+
+        return files_vec;
+    }
+}
+
+
+// General interface
+#[napi]
+impl PackFileHolder
+{
+    pub fn get_text_file(&mut self, filepath: String ) -> &str
+    {    
+        return "test";
+    }
+}
+
+
+
+
